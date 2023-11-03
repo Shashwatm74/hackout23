f51a4006f4f2858936746d55bd74055b5e81ec8e
Shashwat Mishra <shashwat.mishra2022@vitstudent.ac.in>
2023-11-04 03:57:21 +0530

    dashboard updated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -18,7 +18,6 @@ export default function App({ Component, pageProps }) {
   if (dashBoard) {
     return (
       <>
-        <Nav />
         <Component {...pageProps} />
         {/* <Sidebar /> */}
         <Footer />
