84698dfe7c5980778c2bca1f7d0ecfe9fba6d61d
Shashwat Mishra <shashwat.mishra2022@vitstudent.ac.in>
2023-11-04 03:55:56 +0530

    not me work -shash

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,28 +1,93 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "styles/components/dashboard/dashboard.module.scss";
+import Image from 'next/image';
 import { useAuth } from "@/lib/hooks/AuthHook";
+
 function Dashboard() {
     return (
         <>
             <useAuth>
-                <section classname={styles.mainsec}>
-                    <section className={styles.navbar}>
+                <section className={styles.navbar}>
                     <h1 id={styles.logo}>TravelEase</h1>
-                    
-                    <div > 
-                         <ul className={styles.listt}>
+
+                    <div >
+                        <ul className={styles.listt}>
                             <li>Home</li>
                             <li>Cart</li>
                             <li>Contact us</li>
-                         </ul>
+                        </ul>
                     </div>
                     <div className={styles.sub}>
-                        <h3>signup</h3>
-                        <h3>signout</h3>
+                        <ul className={styles.list2}>
+                            <li><button className={styles.sign}>signup</button></li>
+                            <li><button className={styles.sign}>signout</button></li>
+                        </ul>
                     </div>
-            
+
                 </section>
+                <section className={styles.second}>
+                    <div className={styles.trip}>
+                        <div>
+                            <p>Your Trip</p>
+                        </div>
+                        <div className={styles.container}>
+                            <div className={styles.row1}>
+                                <div className={styles.box}>
+                                    <h5>hello</h5>
+                                </div>
+                                <div className={styles.box}>
+                                    <h5>hello</h5>
+                                </div>
+                                <div className={styles.box}>
+                                    <h5>hello</h5>
+                                </div>
+                            </div>
+                            <div className={styles.row1}>
+                                <div className={styles.box}>
+                                    <h5>hello</h5>
+                                </div>
+                                <div className={styles.box}>
+                                    <h5>hello</h5>
+                                </div>
+                                <div className={styles.box}>
+                                    <h5>hello</h5>
+                                </div>
+                            </div>
+                            <div className={styles.search}>
+                                <button className={styles.srch}>Search</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={styles.image}>
+                        <img src="assets/travel.png" alt="Travel Image" />
+
+                    </div>
                 </section>
+                <section className={styles.recom} >
+                    <div className={styles.heading}>Recommended</div>
+                    <div className={styles.product}>
+                        <Image
+                            src={Image}
+                            width={500}
+                            height={500}
+                            alt="Picture of the author"
+                        />
+
+                        <div className={styles.description}>
+                            <h3 className={styles.d1}>Windflower hall</h3>
+                            <p className={styles.d2}>Chharabra, Shimla, Himachal Pradesh</p>
+                            <div className={styles.boxflex}>
+                                <div><p className={styles.d3}>Starting from ₹ 4,000</p></div>
+                                <p>4.3</p>
+                                <p className={styles.d4}>Rating</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </section>
+
             </useAuth>
         </>
     );
