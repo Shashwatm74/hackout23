ebecc66958a4991623560fd4a5df79196737ae2d
Shashwat Mishra <shashwat.mishra2022@vitstudent.ac.in>
2023-11-04 03:49:02 +0530

    conflicts done --shash

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,36 +1,33 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../_app";
+import supabase from "@/lib/db/supabase";
 function Test() {
-    const country = "yourCountry";
-    const currency = "yourCurrency";
-    const locale = "yourLocale";
-    const originPlace = "yourOriginPlace";
-    const destinationPlace = "yourDestinationPlace";
-    const lengthOfStay = "yourLengthOfStay";
-    const outboundPartialDate = "yourOutboundPartialDate";
-    const inboundPartialDate = "yourInboundPartialDate";
-    const apiKey = "yourApiKey";
-    const url = `https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/monthly?destination=%20${destinationPlace}&origin=${originPlace}&length=${lengthOfStay}&currency=${currency}`;
-    const callAPI = async () => {
-        try {
-            const res = await fetch(
-                url, {
-                'X-Access-Token': '<REQUIRED>',
-                'X-RapidAPI-Key': '7a9c655dc0msh8214793a4437402p1827dejsnf31ba0290672',
-                'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com'
-            }
-            );
+    const [fetchError, setFetchError] = useState(null);
+    const [flights, setFlights] = useState(null);
 
-            const data = await res.json();
-            console.log(data);
-        } catch (err) {
-            console.log(err);
+    useEffect(() => {
+        const fetchFlights = async () => {
+            const { data, error } = await supabase
+                .from('flights_ai')
+                .select()
+
+            if (error) {
+                setFetchError('Could not fetch flights')
+                setFlights(null)
+                console.log(error)
+            }
+            if (data) {
+                setFlights(data)
+                setFetchError(null)
+                console.log(data)
+            }
         }
-    };
+        fetchFlights()
+    }, [])
     return (
         <>
             <div>
-                <button onClick={callAPI}>call</button>
+                <button>call</button>
             </div>
         </>
     );
