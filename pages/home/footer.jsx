d6e6c2125ede0049d3fee0f8dd24029eb2d0e5da
Sinchan Shetty <sinchannshetty@gmail.com>
2023-11-04 18:06:51 +0530

    footer done

--- pages/home/footer.jsx
+++ pages/home/footer.jsx
@@ -11,16 +11,50 @@ function FooterHome() {
             <section className={styles.footer}>
                 <div className={styles.name}>
                     <h1>
-                        Itenary.iq
+                        Title.ai
                     </h1>
                     <p>
-                        You Travel, We Care!
+                        A one line tag line a bit longer hehe
                     </p>
                     <p>
-                        Happy Holidays!
+                        Lorem ipsum dolor sit ame
                     </p>
                 </div>
-                <div className={styles.info}>
+                <div className={styles.midsec}>
+                    <div className={styles.home}>
+                    <h1>
+                        Home
+                    </h1>
+                    <p>Blog</p>
+                    <p>Travel</p>
+                    <p>Stay</p>
+                    <p>Bookings</p>
+                    </div>
+                    <div className={styles.policy}>
+                    <h1>
+                        Our Policies
+                    </h1>
+                    <p>Privacy Policy</p>
+                    <p>Terms and Condition</p>
+                    <p>Cancellation Policies</p>
+                    </div>
+                    <div className={styles.info}>
+                    <h1>
+                        Information
+                    </h1>
+                    <p>Office Address</p>
+                    <p>Contact</p>
+                    <p>Deals</p>
+                    </div>
+                </div>
+                <div className={styles.address}>
+                    <div className={styles.teamm}>
+                    <h1>Team</h1>
+                    <p>Harshit Singh</p>
+                    <p>Shashwat Mishra</p>
+                    <p>Sinchan Shetty</p>
+                    <p>Kunal Mukherjee</p>
+                    </div>
                 </div>
             </section>
         </>
